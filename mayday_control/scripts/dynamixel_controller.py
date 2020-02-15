edf5c2fb1798d54ae99e292939b4c7931cd02558
Lasse Simmelsgaard Børresen <fiffigini@gmail.com>
2020-02-16 00:02:37 +0100

    finished functionality to read dxl state and write goal
    I decided to skip ros publishers for now, it was making things more
    complicated, with its datatypes etc. I know it removes the ability to
    interchange with simulation, but that can come later. I want to focus
    on playing with the real robot.
    
    I doctested the rad_to_int_range() and vice versa functions and they
    succeed.
    
    Signed-off-by: Lasse Simmelsgaard Børresen <fiffigini@gmail.com>

--- mayday_control/scripts/dynamixel_controller.py
+++ mayday_control/scripts/dynamixel_controller.py
@@ -2,19 +2,56 @@ import pandas as pd
 import dynamixel_sdk                    # Uses Dynamixel SDK library
 import logging
 import rospy
+import math
+
+
+tau = math.pi * 2
+
 
 FORMAT = '%(asctime)s %(levelname)-8s %(message)s'
 logging.basicConfig(format=FORMAT, level='DEBUG')
 logger = logging.getLogger(__name__)
 
-class dxl_controller:
+
+class DxlController:
+    """
+    Converting back and forth from rad to int range to rad should give the same value back as has been put in
+    >>> t=DxlController()
+    >>> t.rad_to_int_range(t.int_range_to_rad(0, 0, 4095), 0, 4095)
+    0
+
+    >>> t=DxlController()
+    >>> t.rad_to_int_range(t.int_range_to_rad(1, 0, 4095), 0, 4095)
+    1
+
+    >>> t=DxlController()
+    >>> t.rad_to_int_range(t.int_range_to_rad(2048, 0, 4095), 0, 4095)
+    2048
+
+    >>> t=DxlController()
+    >>> t.rad_to_int_range(t.int_range_to_rad(4095, 0, 4095), 0, 4095)
+    4095
+
+    >>> t=DxlController()
+    >>> t.rad_to_int_range(t.int_range_to_rad(0, 0, 4095), 0, 4095)
+    0
+    """
 
     def __init__(self):
 
         self.control_table = pd.read_csv('/home/lb/Mayday/XL430_W250_control_table.csv', sep=';', index_col=3)
         self.dxl_ids = range(1, 18)
 
-        self.packet_handler, self.port_handler = self.init_dxl_communication()
+        self.packet_handler = None
+        self.port_handler = None
+
+    def arm(self):
+        """
+
+        :return:
+        """
+
+        self.init_dxl_communication()
         self.init_dxls()
 
     def init_dxl_communication(self):
@@ -30,33 +67,30 @@ class dxl_controller:
         # Initialize port_handler instance
         # Set the port path
         # Get methods and members of PortHandlerLinux or PortHandlerWindows
-        port_handler = dynamixel_sdk.PortHandler(DEVICENAME)
+        self.port_handler = dynamixel_sdk.PortHandler(DEVICENAME)
 
         # Initialize packet_handler instance
         # Set the protocol version
         # Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
-        packet_handler = dynamixel_sdk.PacketHandler(PROTOCOL_VERSION)
+        self.packet_handler = dynamixel_sdk.PacketHandler(PROTOCOL_VERSION)
 
         # Open port
-        if port_handler.openPort():
-            print("Succeeded to open the port")
+        if self.port_handler.openPort():
+            logger.info("Succeeded to open the port for device: " + DEVICENAME)
         else:
-            print("Failed to open the port")
-            print("Press any key to terminate...")
+            logger.info("Failed to open the port for device: " + DEVICENAME )
+            logger.info("Press any key to terminate...")
             # getch()
             quit()
 
         # Set port baudrate
-        if port_handler.setBaudRate(BAUDRATE):
-            print("Succeeded to change the baudrate")
+        if self.port_handler.setBaudRate(BAUDRATE):
+            logger.info("Succeeded to change the baudrate to: " + str(BAUDRATE))
         else:
-            print("Failed to change the baudrate")
-            print("Press any key to terminate...")
+            logger.info("Failed to change the baudrate to: " + str(BAUDRATE))
+            logger.info("Press any key to terminate...")
             # getch()
             quit()
-
-
-        return packet_handler, port_handler
 
     def init_dxls(self):
         """
@@ -76,21 +110,26 @@ class dxl_controller:
             # Change Velocity Limits
             self.dxl_write(dxl_id, 'Profile Velocity', 16)
 
+            # if second half of dynamixels, revers movement direction to get symmetrical behaviour
+            if dxl_id > len(self.dxl_ids) / 2:
+                self.dxl_write(dxl_id, 'Drive Mode', 1)
+
             # Set position limits
+            logger.warning('position limits not set, commented out.')
             # self.dxl_write(dxl_id, 'Min Position Limit', 1300)
             # self.dxl_write(dxl_id, 'Max Position Limit', 2500)
 
             # Enable torque again.
             self.dxl_write(dxl_id, 'Torque Enable', 1)
 
-    def dxl_write(self, id, name, value):
+    def dxl_write(self, dxl_id, name, value):
         """
         Write value to address of name using dxl_sdk.
         Check input value against min and max given by control table reference.
 
 
         :param self:
-        :param int id: id of dxl
+        :param int dxl_id: dxl_id of dxl
         :param str name: name of control table entry
         :param int value: value to write to control table
         :return: None
@@ -101,11 +140,11 @@ class dxl_controller:
         addr = self.control_table.loc[name, 'Address']
 
         if size == 1:
-            dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(self.port_handler, id, addr, 0)
+            dxl_comm_result, dxl_error = self.packet_handler.write1ByteTxRx(self.port_handler, dxl_id, addr, 0)
         elif size == 2:
-            dxl_comm_result, dxl_error = self.packet_handler.write2ByteTxRx(self.port_handler, id, addr, 0)
+            dxl_comm_result, dxl_error = self.packet_handler.write2ByteTxRx(self.port_handler, dxl_id, addr, 0)
         elif size == 4:
-            dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(self.port_handler, id, addr, 0)
+            dxl_comm_result, dxl_error = self.packet_handler.write4ByteTxRx(self.port_handler, dxl_id, addr, 0)
         else:
             raise Exception('\'size [byte]\' was not 1,2 or 4: ' + str(size))
 
@@ -116,17 +155,17 @@ class dxl_controller:
             logger.error("%s" % self.packet_handler.getRxPacketError(dxl_error))
         if dxl_comm_result == dynamixel_sdk.COMM_SUCCESS and dxl_error == 0:
             logger.debug(
-                '{name} has been successfully been changed to {value} on dxl {id}'
-                .format(name=name, value=value, id=id))
+                '{name} has been successfully been changed to {value} on dxl {dxl_id}'
+                .format(name=name, value=value, dxl_id=dxl_id))
 
-    def dxl_read(self, id, name):
+    def dxl_read(self, dxl_id, name):
         """
         Write value to address of name using dxl_sdk.
         Check input value against min and max given by control table reference.
 
 
         :param self:
-        :param int id: id of dxl
+        :param int dxl_id: dxl_id of dxl
         :param str name: name of control table entry
         :return: value read from control table
         :rtype: int
@@ -136,11 +175,11 @@ class dxl_controller:
         addr = self.control_table.loc[name, 'Address']
 
         if size == 1:
-            value, dxl_comm_result, dxl_error = self.packet_handler.read1ByteTxRx(self.port_handler, id, addr)
+            value, dxl_comm_result, dxl_error = self.packet_handler.read1ByteTxRx(self.port_handler, dxl_id, addr)
         elif size == 2:
-            value, dxl_comm_result, dxl_error = self.packet_handler.read2ByteTxRx(self.port_handler, id, addr)
+            value, dxl_comm_result, dxl_error = self.packet_handler.read2ByteTxRx(self.port_handler, dxl_id, addr)
         elif size == 4:
-            value, dxl_comm_result, dxl_error = self.packet_handler.read4ByteTxRx(self.port_handler, id, addr)
+            value, dxl_comm_result, dxl_error = self.packet_handler.read4ByteTxRx(self.port_handler, dxl_id, addr)
         else:
             raise Exception('\'size [byte]\' was not 1,2 or 4: ' + str(size))
 
@@ -151,30 +190,125 @@ class dxl_controller:
             logger.error("%s" % self.packet_handler.getRxPacketError(dxl_error))
         if dxl_comm_result == dynamixel_sdk.COMM_SUCCESS and dxl_error == 0:
             logger.debug(
-                '{name} has been successfully been read as {value} on dxl {id}'
-                .format(name=name, value=value, id=id))
+                '{name} has been successfully been read as {value} on dxl {dxl_id}'
+                .format(name=name, value=value, dxl_id=dxl_id))
 
+    def rad_to_int_range(self, value_rad, range_min, range_max):
+        """
+        Tranform a radian value to one in an integer range
 
+        0 radians will map to middle of range.
 
+        :param float value_rad:
+        :param int range_min:
+        :param int range_max:
+        :return: value specified in the configured range
+        :rtype: int
 
+        0 should translate to range_min
+        >>> t = DxlController()
+        >>> t.rad_to_int_range(0.0, 0, 4095)
+        0
 
+        tau/2 should give center of range
+        >>> t = DxlController()
+        >>> t.rad_to_int_range(tau / 2, 0, 4095)
+        2048
 
-def main():
-    """
-    Connect to all dynamixels and read from them periodically.
-    :return:
-    """
+        To get 1 out, the rad input should tau * 1 / range
+        >>> t = DxlController()
+        >>> t.rad_to_int_range(tau * 1 / (4095 - 0), 0, 4095)
+        1
 
+        To get the highest value of the interval, the rad input should be: tau -  tau * 1/(range)
+        >>> t = DxlController()
+        >>> t.rad_to_int_range(tau - tau * 1 / (4095 - 0), 0, 4095)
+        4095
+        """
 
+        if range_min >= range_max:
+            raise ValueError(
+                'Range_min must be less than range_max: range_min: {}, range_max: {}'.format(range_min, range_max))
 
-    dxl_ctrler = dxl_controller()
+        if  value_rad < 0.0 or value_rad >= tau:
+            raise ValueError(
+                'Value_rad must be within (0, tau), value_rad: {}'
+                .format(value_rad))
 
-    rate = rospy.Rate(10)
+        return int(round((value_rad % tau) * (range_max - range_min + 1) / tau))
 
-    while not rospy.is_shutdown():
+    def int_range_to_rad(self, value_int, range_min, range_max):
+        """
+        Tranform a radian value to one in an integer range
 
-        rate.sleep()
+        0 radians will map to middle of range.
+
+        :param float value_rad:
+        :param int range_min:
+        :param int range_max:
+        :return: value specified in the configured range
+        :rtype: int
+
+        0 should translate to 0.0 rad
+        >>> t = DxlController()
+        >>> t.int_range_to_rad(0, 0, 4095)
+        0.0
+
+        halfway through the range should tau/2
+        >>> t = DxlController()
+        >>> t.int_range_to_rad(2048, 0, 4095) == tau/2
+        True
+        """
+
+        if range_min >= range_max:
+            raise ValueError(
+                'Range_min must be less than range_max: range_min: {}, range_max: {}'.format(range_min, range_max))
+
+        if  value_int < range_min or value_int > range_max:
+            raise ValueError(
+                'Value_int must be within range: range_min: {}, range_max: {}, value_int: {}'
+                .format(range_min, range_max, value_int))
+
+        return value_int *  tau / (range_max - range_min + 1)
+
+    def read_dxl_state(self, dxl_id):
+        """
+
+        :param dxl_id:
+        :return: state
+        :rtype: dict
+        """
+
+        # Unit;Value;Range
+        # rpm, 0.229, [0 ~ 1, 023]
+
+        state = {}
+        state['pos'] = self.int_range_to_rad(self.dxl_read(dxl_id, 'Present_Position'), 0, 4095)
+
+        #  Values from 0 ~ 1023, measured in unit 0.229 rpm, converted to rad/s
+        state['vel'] = self.dxl_read(dxl_id, 'Present_Velocity') * 0.229 * tau / 60
+
+        #  Values from -1000 ~ 1000, measured in unit 0.1 %. Load is directional positive values are CCW
+        state['torq'] = self.dxl_read(dxl_id, 'Present_Load')
+
+        #  Values from 0 ~ 100, measured in unit 1 degC
+        # Temperature could be used by the robot to move less, like it was getting tired, or was sweating.
+        state['temp'] = self.dxl_read(dxl_id, 'Present_Temperature')
+
+        return state
+
+    def write_dxl_goal_pos(self, dxl_id, goal_pos):
+        """
+        Write goal position, translating from radians to range defined by dxl controltable, i.e. 0-4095
+
+        :param dxl_id:
+        :param goal_pos: angular position in radians
+        """
+
+        self.dxl_write(dxl_id, 'Goal Position', self.rad_to_int_range(goal_pos, 0, 4095))
 
 
 if __name__ == '__main__':
-    main()
+    import doctest
+    doctest.testmod()
+
