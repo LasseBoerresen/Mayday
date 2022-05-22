683fe327544d647fbc184a0d791eeef8eb4f62aa
lb <lb@informetics.com>
2022-05-22 19:47:04 +0200

    refac, extracted methods for LegFactory
    Signed-off-by: lb <lb@informetics.com>

--- mayday_control/scripts/leg_factory.py
+++ mayday_control/scripts/leg_factory.py
@@ -1,27 +1,43 @@
+from typing import List
+
 from dynamixel_adapter import DynamixelAdapter
 from leg import Leg
 from dxl_motor import DxlMotor
 from motor_state import MotorState
 
 
-class LegFactory(object):
+class LegFactory:
+    N_JOINTS = 3
+
     def __init__(self, adapter: DynamixelAdapter):
         self.adapter = adapter
 
     # TODO maybe base id should be starting id, and then joint 0 just has that as dxl_id
     def create_basic(self, base_id: int, side: str) -> Leg:
-        N_JOINTS = 3
-        joints = []
-        for joint_num in range(N_JOINTS):
-            id_num = base_id * 3 + joint_num + 1
-            if joint_num == 0:
-                drive_mode = 'forward' if side == 'left' else 'backward'
-            elif joint_num == 1:
-                drive_mode = 'backward'
-            elif joint_num == 2:
-                drive_mode = 'forward'
-            else:
-                raise Exception(f'Joint number not recognized, got {joint_num}')
-            joints.append(DxlMotor(id_num, self.adapter, MotorState(), drive_mode))
-
+        joints = self._create_joints(base_id, side)
         return Leg(joints)
+
+    def _create_joints(self, base_id: int, side: str) -> List[DxlMotor]:
+        return [self._create_joint(base_id, joint_num, side) for joint_num in range(self.N_JOINTS)]
+
+    def _create_joint(self, base_id: int, joint_num: int, side) -> DxlMotor:
+        id = self._motor_id(base_id, joint_num)
+        drive_mode = self._drive_mode(joint_num, side)
+        return DxlMotor(id, self.adapter, MotorState(), drive_mode)
+
+    @staticmethod
+    def _motor_id(base_id: int, joint_num: int) -> int:
+        return base_id * 3 + joint_num + 1
+
+    @staticmethod
+    def _drive_mode(joint_num: int, side: str) -> str:
+        if joint_num == 0:
+            drive_mode = 'forward' if side == 'left' else 'backward'
+        elif joint_num == 1:
+            drive_mode = 'backward'
+        elif joint_num == 2:
+            drive_mode = 'forward'
+        else:
+            raise Exception(f'Joint number not recognized, got {joint_num}')
+
+        return drive_mode
