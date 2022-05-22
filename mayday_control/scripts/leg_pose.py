76688bfc7bbae4b6a8eec2573c1beb7963b40a42
lb <lb@informetics.com>
2022-05-22 23:40:07 +0200

    fixed typing issue by calling super().__new__first

--- mayday_control/scripts/leg_pose.py
+++ mayday_control/scripts/leg_pose.py
@@ -1,5 +1,6 @@
 class LegPose(tuple):
     def __new__(cls, iterable):
-        if len(iterable) != 3:
+        new = super().__new__(cls, iterable)
+        if len(new) != 3:
             raise ValueError(f'joint_angles must have 3 values, got: {iterable}')
-        return super().__new__(cls, iterable)
+        return new
