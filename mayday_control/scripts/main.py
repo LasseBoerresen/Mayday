f364e4b0522a68ab5cb9a31f22f9678a68679077
lb <lb@informetics.com>
2022-05-22 18:35:36 +0200

    removed left over parameter
    Signed-off-by: lb <lb@informetics.com>

--- mayday_control/scripts/main.py
+++ mayday_control/scripts/main.py
@@ -16,7 +16,7 @@ def create_mayday() -> MaydayRobot:
     dxl_adapter = DynamixelAdapter(dxl_port_adapter)  # Todo why does this not just init communication?
     dxl_adapter.port_adapter.init_communication()
     leg_factory = LegFactory(dxl_adapter)
-    mayday_factory = MaydayRobotFactory(leg_factory, dxl_adapter)
+    mayday_factory = MaydayRobotFactory(leg_factory)
     may = mayday_factory.create_basic()
 
     return may
