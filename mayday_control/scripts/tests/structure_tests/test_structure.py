e3d29549564727c9af7624c513aba3ab73695b4c
Lasse Børresen <110817343+LasseBoerresenAtDryp@users.noreply.github.com>
2023-12-12 20:07:22 +0100

    🎨 parameterized duplicate test and auto reformated test to correct indentation of parameterizatoin

--- mayday_control/scripts/tests/structure_tests/test_structure.py
+++ mayday_control/scripts/tests/structure_tests/test_structure.py
@@ -7,7 +7,7 @@ from geometry.pose import Pose
 from geometry.vec3 import Vec3
 from physical_quantities.angle import Angle
 from side import LeftSide, RightSide, Side
-from structure.components import CoxaJoint, CoxaMotorLink, CoxaMotorAttachment, FemurJoint, FemurMotorAttachment
+from structure.components import CoxaJoint, CoxaMotorAttachment, FemurJoint, FemurMotorAttachment, Joint
 from structure.leg_position import BackLegPosition, CenterLegPosition, FrontLegPosition, LegPosition
 from structure.structure import Structure
 from tests.structure_tests.fake_motor import FakeMotor
@@ -29,19 +29,17 @@ class TestStructure:
         expected = Pose.zeros()
         assert actual == expected
 
-    @pytest.mark.parametrize(
-        "side,leg_position,expected_pose",
-        [
-            (RightSide, CenterLegPosition, Pose(Vec3(0, 0.086, 0), Vec3(0, 0, -tau / 4))),
-            (RightSide, FrontLegPosition, Pose(Vec3(0.077, 0.074, 0), Vec3(0, 0, -tau / 4 - Angle.from_deg(55)))),
-            (RightSide, BackLegPosition, Pose(Vec3(-0.077, 0.074, 0), Vec3(0, 0, -tau / 4 + Angle.from_deg(55)))),
-            (LeftSide, CenterLegPosition, Pose(Vec3(0, -0.086, 0), Vec3(0, 0, tau / 4))),
-            (LeftSide, FrontLegPosition, Pose(Vec3(0.077, -0.074, 0), Vec3(0, 0, tau / 4 + Angle.from_deg(55)))),
-            (LeftSide, BackLegPosition, Pose(Vec3(-0.077, -0.074, 0), Vec3(0, 0, tau / 4 - Angle.from_deg(55)))),
-        ])
+    @pytest.mark.parametrize("side, leg_position, expected_pose",
+    [
+        (RightSide, CenterLegPosition, Pose(Vec3(0, 0.086, 0), Vec3(0, 0, -tau / 4))),
+        (RightSide, FrontLegPosition, Pose(Vec3(0.077, 0.074, 0), Vec3(0, 0, -tau / 4 - Angle.from_deg(55)))),
+        (RightSide, BackLegPosition, Pose(Vec3(-0.077, 0.074, 0), Vec3(0, 0, -tau / 4 + Angle.from_deg(55)))),
+        (LeftSide, CenterLegPosition, Pose(Vec3(0, -0.086, 0), Vec3(0, 0, tau / 4))),
+        (LeftSide, FrontLegPosition, Pose(Vec3(0.077, -0.074, 0), Vec3(0, 0, tau / 4 + Angle.from_deg(55)))),
+        (LeftSide, BackLegPosition, Pose(Vec3(-0.077, -0.074, 0), Vec3(0, 0, tau / 4 - Angle.from_deg(55)))),
+    ])
     def test__given_CoxaMotorAttachment__when_get_origin__then_returns_expected_pose(
             self, side: Side, leg_position: LegPosition, expected_pose: Pose):
-
         # Given
         joint = CoxaMotorAttachment(side, leg_position)
 
@@ -51,37 +49,26 @@ class TestStructure:
         # Then
         assert actual == expected_pose
 
-    def test__given_CoxaJoint__when_get_origin__then_returns_pose_zeros(self):
+    @pytest.mark.parametrize('joint, expected_pose',
+    [
+        (CoxaJoint(FakeMotor()), Pose.zeros()),
+        (FemurMotorAttachment(), Pose(Vec3(0.032, 0, -0.011), Vec3(tau / 4, 0, tau / 4))),
+        (FemurJoint(FakeMotor()), Pose.zeros())
+    ])
+    def test__given_CoxaJoint__when_get_origin__then_returns_expected_pose(self, joint: Joint, expected_pose: Pose):
         # Given
-        joint = CoxaJoint(FakeMotor())
 
         # When
         actual = joint.origin
 
         # Then
-        assert actual == Pose.zeros()
+        assert actual == expected_pose
 
-    def test__given_FemurMotorAttachment__when_get_origin__then_returns_expected_pose(self):
-        # Given
-        joint = FemurMotorAttachment()
-
-        # When
-        actual = joint.origin
-
-        # Then
-        assert actual == Pose(Vec3(0.032, 0, -0.011), Vec3(tau/4, 0, tau/4))
-
-    def test__given_FemurJoint__when_get_origin__then_returns_pose_zeros(self):
-        # Given
-        joint = FemurJoint(FakeMotor())
-
-        # When
-        actual = joint.origin
-
-        # Then
-        assert actual == Pose.zeros()
-
-    @pytest.mark.parametrize('joint_type', [CoxaJoint, FemurJoint])
+    @pytest.mark.parametrize('joint_type',
+    [
+        CoxaJoint,
+        FemurJoint
+    ])
     def test__given_RotaionalJoint__when_get_axis__then_returns_z(self, joint_type: type):
         # Given
         joint = joint_type(FakeMotor())
@@ -94,6 +81,7 @@ class TestStructure:
 
 
 # TODO TDD simplest cases of forward Kinematics test
+# TODO TDD printing meaninful name of each component, based on its parents.
 
 if __name__ == '__main__':
     pytest.main()
