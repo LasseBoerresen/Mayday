03dbc45b89d7a459dbfce5c2daed352213f38918
Lasse Børresen <110817343+LasseBoerresenAtDryp@users.noreply.github.com>
2023-12-13 19:45:04 +0100

    split structure and maydayStructure, to have something that is easier to test with generic values

--- mayday_control/scripts/structure/structure.py
+++ mayday_control/scripts/structure/structure.py
@@ -1,9 +1,28 @@
 from geometry.pose import Pose
 from geometry.vec3 import Vec3
+from side import Side
+from structure.components import Joint, Link
+from structure.leg_position import LegPosition
 
 
-class Structure:
-    def get_thorax_position(self) -> Pose:
+class Structure():
+    def __init__(self, links: [Link], joints: [Joint]):
+        pass
+
+    def get_pose_of(self, ):
+        pass
+
+
+class MaydayStructure(Structure):
+    def get_thorax_pose(self) -> Pose:
         return Pose.zeros()
         # TODO fix cheat value
+
+    def get_leg_root_pose(self, leg_position: LegPosition, side: Side) -> Pose:
+        return self._get_leg(leg_position, side).origin
+
+    # def _get_leg(self, leg_position: LegPosition, side: Side) -> Leg:
+    #     return self._legs[(leg_position, side)]
+
+
 
