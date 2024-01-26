dddb1a8bc003a7171a1646f399c516258111dd92
Lasse Børresen <110817343+LasseBoerresenAtDryp@users.noreply.github.com>
2024-01-26 18:02:38 -0500

    ✨ untested implementations of Rpy and Q (quarternion for rotation) as facades to Numerics.Quaternion.

---
+++ mayday/Geometry/Q.cs
@@ -0,0 +1,69 @@
+﻿using System.Numerics;
+using UnitsNet;
+
+namespace mayday.Geometry;
+
+/// <summary>
+/// Rotation Quaternion
+/// </summary>
+public record Q(
+    double W,
+    double X,
+    double Y,
+    double Z)
+{
+    // Note: angle of a quaternion is only half a rotation by that quaternion!
+    private Angle Angle => Angle.FromRadians(2.0 * Math.Acos(W));
+
+    private Vector3 Axis => Vector3.Normalize(Vector3.Normalize(Xyz));
+
+    private Vector3 Xyz => new((float)X, (float)Y, (float)Z);
+
+    public static Q FromRpy(Rpy rpy)
+    {
+        // Quaternion uses different definition of rpy axes, specifically zxy
+        var nq = Quaternion.CreateFromYawPitchRoll((float)rpy.P.Radians, (float)rpy.R.Radians, (float)rpy.Y.Radians);
+        return FromNumericsQ(nq);
+    }
+
+    public Rpy ToRpy()
+    {
+        throw new NotImplementedException();
+        // return new Rpy()
+    }
+
+
+    public static Q operator -(Q a, Q b)
+    {
+        var relativeRotation = Quaternion.Multiply(Quaternion.Inverse(a.ToNumericsQ()), b.ToNumericsQ());
+        return FromNumericsQ(relativeRotation);
+    }
+
+    public static Q operator *(Q q, double multiplier)
+    {
+        return FromAxisAngle(q.Axis, q.Angle * multiplier);
+    }
+
+    public static Q FromAxisAngle(Vector3 axis, Angle angle)
+    {
+        return FromNumericsQ(Quaternion.CreateFromAxisAngle(axis, (float)angle.Radians));
+    }
+
+    private static Q FromNumericsQ(Quaternion nq)
+    {
+        return new(
+            nq.W,
+            nq.X,
+            nq.Y,
+            nq.Z);
+    }
+
+    private Quaternion ToNumericsQ()
+    {
+        return new(
+            (float)X,
+            (float)Y,
+            (float)Z,
+            (float)W);
+    }
+}
--- mayday/Geometry/Rpy.cs
+++ mayday/Geometry/Rpy.cs
@@ -1,8 +1,6 @@
 ﻿// using System.Numerics;
 
-using System.Numerics;
 using UnitsNet;
-using UnityEngine;
 
 namespace mayday.Geometry;
 
@@ -22,73 +20,14 @@ namespace mayday.Geometry;
 /// <param name="Y">Yaw</param>
 public record Rpy(Angle R, Angle P, Angle Y)
 {
-    public Rpy(double r, double p, double y)
-        : this(Angle.FromRevolutions(r), Angle.FromRevolutions(p), Angle.FromRevolutions(y)) {}
-
     public static Rpy Zero => new(0, 0, 0);
 
     public static Rpy One => new(1, 1, 1);
 
-    public static Rpy operator -(Rpy rpy, Rpy other)
-    {
-        var qThis = rpy.AsQuaternion();
-        var qOther = other.AsQuaternion();
-        var relativeRotation = Quaternion.Multiply(Quaternion.Inverse(qThis), qOther);
-        return Rpy.FromQuaternion(relativeRotation);
-    }
+    public Rpy(double r, double p, double y)
+        : this(Angle.FromRevolutions(r), Angle.FromRevolutions(p), Angle.FromRevolutions(y)) { }
 
-    public static Rpy operator *(Rpy rpy, double multiplier)
-    {
-        return (Q.FromRpy(rpy) * multiplier).AsRpy();
-    }
-}
+    public static Rpy operator -(Rpy a, Rpy b) => (Q.FromRpy(a) - Q.FromRpy(b)).ToRpy();
 
-/// <summary>
-/// Rotation Quaternion
-/// </summary>
-public record Q(
-    double W,
-    double X,
-    double Y,
-    double Z)
-{
-    public static Q FromRpy(Rpy rpy)
-    {
-        // Quaternion uses different definition of rpy axes, specifically zxy
-        var nq = Quaternion.CreateFromYawPitchRoll((float)rpy.P.Radians, (float)rpy.R.Radians, (float)rpy.Y.Radians);
-        return FromNq(nq);
-    }
-
-    public Rpy ToRpy()
-    {
-        throw new NotImplementedException();
-        // return new Rpy()
-    }
-
-    public static Q operator *(Q q, double multiplier)
-    {
-        return FromAxisAngle(q.Axis, q.Angle * multiplier);
-    }
-
-    // Note: angle of a quaternion is only half a rotation by that quaternion!
-    private Angle Angle => Angle.FromRadians(2.0 * Math.Acos(W));
-
-    private Vector3 Axis => Vector3.Normalize(Vector3.Normalize(Xyz));
-
-    public static Q FromAxisAngle(Vector3 axis, Angle angle)
-    {
-        return FromNq(Quaternion.CreateFromAxisAngle(axis, (float)angle.Radians));
-    }
-
-    private Vector3 Xyz => new((float)X, (float)Y, (float)Z);
-
-    private static Q FromNq(Quaternion nq)
-    {
-        return new(nq.W, nq.X, nq.Y, nq.Z);
-    }
-
-    private Quaternion ToNq()
-    {
-        return new((float)X, (float)Y, (float)Z, (float)W);
-    }
+    public static Rpy operator *(Rpy rpy, double multiplier) => (Q.FromRpy(rpy) * multiplier).ToRpy();
 }
