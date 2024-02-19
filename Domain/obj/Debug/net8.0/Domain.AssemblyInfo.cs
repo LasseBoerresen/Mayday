19fe08a5fc2bbaa833b3baa4697651b16be43722
Lasse Børresen <110817343+LasseBoerresenAtDryp@users.noreply.github.com>
2024-02-19 23:23:36 +0100

    autogenerated junk

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9518728db3a78a741bbd3bfbb04bd5ceeda56718")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c8680b333c1754e5297ced4586c756136f9f36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
