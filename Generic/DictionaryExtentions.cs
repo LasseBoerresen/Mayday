e51dcfc605bcbd6edc2934504c347af7bcd9b579
LB <fiffigini@gmail.com>
2024-06-16 22:27:05 +0200

    ✨ Added Geneircs project, with dictionaryExtention for Select value, which was clumsy.

---
+++ Generic/DictionaryExtentions.cs
@@ -0,0 +1,13 @@
+﻿namespace Generic;
+
+public static class DictionaryExtentions
+{
+    public static IDictionary<TKey, TValueNew> MapValue<TKey, TValue, TValueNew>(
+        this IDictionary<TKey, TValue> dictionary,
+        Func<TValue, TValueNew> mapper) where TKey : notnull
+    {
+            return dictionary
+            .Select(kvp => new KeyValuePair<TKey,TValueNew>(kvp.Key, mapper(kvp.Value)))
+            .ToDictionary();
+        }
+}
