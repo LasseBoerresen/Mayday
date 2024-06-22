c2c46e363097505ed8d88a5d095fc6087f99579e
LB <fiffigini@gmail.com>
2024-06-23 00:06:52 +0200

    🧪 Expanded port test to checking all dynamixels

--- Test/Integration/Dynamixel/PortAdapterSdkImplTests.cs
+++ Test/Integration/Dynamixel/PortAdapterSdkImplTests.cs
@@ -8,39 +8,46 @@ namespace Test.Integration.Dynamixel;
 public class PortAdapterSdkImplTests
 {
     readonly PortAdapterSdkImpl _portAdapter = new();
-    readonly Id _id = new(1);
+    Id _id = new(1);
 
     [Fact]
-    void GivenRobotIsAttachedAndId1_WhenSetTwoGoalAnglesAndSleep1sBetween_ThenCurrentPositionWithin20()
+    void GivenRobotIsAttached_WhenSetTwoGoalAnglesAndSleep1sBetween_ThenCurrentPositionWithin20()
     {
         // Given
         _portAdapter.Initialize();
         _portAdapter.Write(_id, ControlRegister.TorqueEnable, Convert.ToByte(true));
+    
+        foreach (var i in Enumerable.Range(1, 18))
+        {
+            _id = new(i);
+            _portAdapter.Write(_id, ControlRegister.TorqueEnable, Convert.ToByte(true));
+            
+            // When
+            var goal = PositionAngle.StepCenter - 100;
+            SetAndWaitForGoal(goal);
 
-        // When
-        var goal = PositionAngle.StepCenter + 300;
-        SetAndWaitForGoal(goal);
+            // Then
+            AssertGoalReached(goal);
 
-        // Then
-        AssertGoalReached(goal);
-
-        // When Again
-        goal = PositionAngle.StepCenter;
-        SetAndWaitForGoal(goal);
-        
-        // Then again
-        AssertGoalReached(goal);
+            // When Again
+            goal = PositionAngle.StepCenter;
+            SetAndWaitForGoal(goal);
+            
+            // Then again
+            AssertGoalReached(goal);
+        }
     }
 
     void SetAndWaitForGoal(uint goal)
     {
         _portAdapter.Write(_id, ControlRegister.GoalPosition, goal);
-        Thread.Sleep(TimeSpan.FromSeconds(1));
+        
+        Thread.Sleep(TimeSpan.FromSeconds(0.1));
     }
 
     void AssertGoalReached(uint goal)
     {
-        const int toleranceInAngleSteps = 20;
+        const int toleranceInAngleSteps = 30;
         
         var currentPosition = _portAdapter.Read(_id, ControlRegister.GoalPosition);
         var absoluteDifference = Math.Abs((int)currentPosition - goal);
