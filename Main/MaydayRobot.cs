3e78a37def86b2c1eb78c987895f2be44d470977
LB <fiffigini@gmail.com>
2024-06-23 17:02:09 +0200

    ✨ Extracted MaydayRobot Class from main.Program.

---
+++ Main/MaydayRobot.cs
@@ -0,0 +1,38 @@
+﻿using Dynamixel;
+using ManualBehavior;
+using MaydayDomain;
+using MaydayDomain.MotionPlanning;
+using RobotDomain.Behavior;
+using RobotDomain.Structures;
+
+namespace Main;
+
+public class MaydayRobot(BehaviorController behaviorController)
+{
+    public static MaydayRobot Create()
+    {
+        JointFactory jointFactory = CreateJointFactory();
+
+        var structure = MaydayStructure.Create(jointFactory);
+
+        MaydayMotionPlanner maydayMotionPlanner = new InstantPostureMaydayMotionPlanner(structure);
+        BehaviorController behaviorController = new TerminalPostureBehaviorController(maydayMotionPlanner);
+
+        return new(behaviorController);
+
+    }
+
+    public void Start()
+    {
+        behaviorController.Start();
+    }
+
+    static JointFactory CreateJointFactory()
+    {
+        PortAdapter portAdapterSdkImpl = new PortAdapterSdkImpl();
+        
+        Adapter jointAdapter = new AdapterSdkImpl(portAdapterSdkImpl);
+        
+        return new DynamixelJointFactory(jointAdapter);
+    }
+}
--- Main/Program.cs
+++ Main/Program.cs
@@ -1,17 +1,5 @@
-﻿using Dynamixel;
-using ManualBehavior;
-using MaydayDomain;
-using MaydayDomain.MotionPlanning;
-using RobotDomain.Behavior;
-using RobotDomain.Structures;
+﻿using Main;
 
-PortAdapter portAdapterSdkImpl = new PortAdapterSdkImpl();
-Adapter jointAdapter = new AdapterSdkImpl(portAdapterSdkImpl);
-JointFactory jointFactory = new DynamixelJointFactory(jointAdapter);
-
-var structure = MaydayStructure.Create(jointFactory);
-
-MaydayMotionPlanner maydayMotionPlanner = new InstantPostureMaydayMotionPlanner(structure);
-BehaviorController behaviorController = new TerminalPostureBehaviorController(maydayMotionPlanner);
-
-behaviorController.Start();
+MaydayRobot
+    .Create()
+    .Start();
