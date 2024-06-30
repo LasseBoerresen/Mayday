51aebe53d24356a336f6a4a22dd04ba8c41eb24a
LB <fiffigini@gmail.com>
2024-06-30 11:53:26 +0200

    🎨 Using expresion body for simplicity

--- MaydayDomain/MotionPlanning/InstantPostureMaydayMotionPlanner.cs
+++ MaydayDomain/MotionPlanning/InstantPostureMaydayMotionPlanner.cs
@@ -2,23 +2,11 @@
 
 namespace MaydayDomain.MotionPlanning;
 
-public class InstantPostureMaydayMotionPlanner : MaydayMotionPlanner
+public class InstantPostureMaydayMotionPlanner(MaydayStructure structure) : MaydayMotionPlanner
 {
-    readonly MaydayStructure _structure;
-
-    public InstantPostureMaydayMotionPlanner(MaydayStructure structure)
-    {
-        _structure = structure;
-    }
-
-    public override MaydayStructurePosture GetPosture()
-    {
+    public override MaydayStructurePosture GetPosture() => 
         throw new NotImplementedException();
-    }
 
-    public override void SetPosture(MaydayStructurePosture posture)
-    {
-        // TODO TDD
-        _structure.SetPosture(posture);
-    }
+    public override void SetPosture(MaydayStructurePosture posture) => 
+        structure.SetPosture(posture);
 }
