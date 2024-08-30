852bd6e222311c9c8aee074268838dbe5c222a9e
LB <fiffigini@gmail.com>
2024-08-30 21:32:42 +0200

    🧪 failing test, transforms should add xyz in new coordinate frame after first rotation

--- Test/Unit/RobotDomain/Geometry/TransformTests.cs
+++ Test/Unit/RobotDomain/Geometry/TransformTests.cs
@@ -21,6 +21,11 @@ public class TransformTests
             {"0", Transform.Zero, Transform.Zero, Transform.Zero},
             {"1", Transform.FromXyz(new(1, 0, 0)), Transform.Zero, Transform.FromXyz(new(1, 0, 0))},
             {"2", Transform.FromXyz(new(1, 0, 0)), Transform.FromXyz(new(1, 0, 0)), Transform.FromXyz(new(2, 0, 0))},
+            {
+                "3, quarter yaw: 1x -> 1y", 
+                Transform.FromQ(Q.FromRpy(new(0, 0, 0.25))), 
+                Transform.FromXyz(new(1, 0, 0)), 
+                new Transform(new(0, 1, 0), Q.FromRpy(new(0, 0, 0.25)))},
         };
     }
 
