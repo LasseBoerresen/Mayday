d55d297636c54b9cf89d72d9b47beeb4750a354f
LB <fiffigini@gmail.com>
2024-09-08 19:28:05 +0200

    🔥 Removed old copied values from python

--- Dynamixel/AdapterSdkImpl.cs
+++ Dynamixel/AdapterSdkImpl.cs
@@ -10,12 +10,7 @@ public class AdapterSdkImpl(PortAdapter portAdapter) : Adapter
 {
     static int DXL_BROADCAST_ID = 254;
     
-    static RotationalAcceleration _ACCELERATION_STEP_SIZE = UnitsNetExtensions.RotationalAccelerationFromRpm2(214.577);
-    static Ratio _LOAD_STEP_SIZE = Ratio.FromDecimalFractions(0.1);
-    static TemperatureDelta _TEMPERATURE_STEP_SIZE = TemperatureDelta.FromDegreesCelsius(1.0);
-    static Ratio _TORQ_LIMIT_REST = Ratio.FromDecimalFractions(1.0);
     static Option<RotationalSpeed> _velocityLimitSlow = RotationalSpeed.FromRevolutionsPerSecond(0.5);  // AngularVelocity(tau / 8)  // tau / 16;
-    static Option<RotationalAcceleration> _ACC_LIMIT_SLOW = Option<RotationalAcceleration>.None;  // tau / 8;
     static int _POSITION_P_GAIN_SOFT = 200; // 640;  // 200;
     static int _POSITION_I_GAIN_SOFT = 300;
     static int _POSITION_D_GAIN_SOFT = 4000;
