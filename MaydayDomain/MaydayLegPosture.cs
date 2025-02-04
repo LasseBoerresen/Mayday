7bb347882ea9039f92b6cb4444053000fcf32890
LB <fiffigini@gmail.com>
2025-02-04 17:58:41 +0100

    🚧 saving edited values, but dont know if they are more correct

--- MaydayDomain/MaydayLegPosture.cs
+++ MaydayDomain/MaydayLegPosture.cs
@@ -13,8 +13,8 @@ public record MaydayLegPosture(Angle CoxaAngle, Angle FemurAngle, Angle TibiaAng
     
     public static MaydayLegPosture Neutral => new(0.0, 0.0, 0.0);
     public static MaydayLegPosture NeutralWithBackTwist => new(0.125, 0.0, 00.0);
-    public static MaydayLegPosture Straight => new(0.0, -0.0625, 0.25);
-    public static MaydayLegPosture StraightWithBackTwist => new(0.125, -0.0625, 0.25);
+    public static MaydayLegPosture Straight => new(0.0, 0.0625, -0.25); // TODO Should be non negative, moving femur down is a positive rotation
+    public static MaydayLegPosture StraightWithBackTwist => new(0.125, 0.0625, -0.25);
     public static MaydayLegPosture Sitting => new(0.0, 0.3, -0.2);
     public static MaydayLegPosture SittingTall => new(0.0, 0.3, -0.15);
     public static MaydayLegPosture Standing => new(0.0, 0.21, -0.26);
