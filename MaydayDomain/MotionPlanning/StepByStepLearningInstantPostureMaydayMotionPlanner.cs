eaa766bb2293b82f8586e90c770d3300865c9bcc
LB <fiffigini@gmail.com>
2025-02-15 22:40:47 +0100

    ✨ Added bypass version of MoveTipPositions to simply see mayday twitch.
    Clearly, getting data and sending to dynamixels is the bottleneck. Need to do batching and caching of values.
    
    Also, need to handle when motors get overloaded. Should not crash completely, but simply reset angle and restart the dyna.

--- MaydayDomain/MotionPlanning/StepByStepLearningInstantPostureMaydayMotionPlanner.cs
+++ MaydayDomain/MotionPlanning/StepByStepLearningInstantPostureMaydayMotionPlanner.cs
@@ -1,6 +1,7 @@
 ﻿using Generic;
 using RobotDomain.Geometry;
 using RobotDomain.Structures;
+using UnitsNet;
 
 namespace MaydayDomain.MotionPlanning;
 
@@ -31,6 +32,36 @@ public class StepByStepLearningInstantPostureMaydayMotionPlanner
 
         var errors = FromLegProperties(expectedPositions, actualPositions);
         var trainingDataPoints = ToTrainingDataPoints(inputs, outputs, errors);
+        // Just predicting for now, to get the model started.
+        // Once it can predict random values, they we can start training 
+        // _neuralNetwork.Train(trainingDataPoints);
+    }
+    
+    public void MoveTipPositionsBypassNN(MaydayStructureSet<Xyz> tipDeltas)
+    {
+        var inputs = tipDeltas.Map(CreateInput);
+        var expectedPositions = inputs.Map(i => i.EndXyz);
+
+        // hack to do small twitch movements, bypassing the untrained network. 
+        var outputs = inputs.Map(i => i.StartPosture).Zip(tipDeltas)
+            .Select(zip => new MaydayLegPosture(
+                zip.First.CoxaAngle + Angle.FromRevolutions(zip.Second.X.Meters),
+                zip.First.FemurAngle + Angle.FromRevolutions(zip.Second.Y.Meters),
+                zip.First.TibiaAngle + Angle.FromRevolutions(zip.Second.Z.Meters)))
+            .ToMaydayStructureSet();
+            
+        
+        // var outputs = inputs.Map(_neuralNetwork.Predict);
+        
+        Console.WriteLine("outputs: \n" + outputs);
+        Structure.SetPosture(MaydayStructurePosture.FromSet(outputs));
+        // SetPostures(outputs);
+        WaitForMovementToFinish();
+
+        var actualPositions = Structure.GetPositionsOf(LinkName.Tip);
+
+        var errors = FromLegProperties(expectedPositions, actualPositions);
+        // var trainingDataPoints = ToTrainingDataPoints(inputs, outputs, errors);
         // Just predicting for now, to get the model started.
         // Once it can predict random values, they we can start training 
         // _neuralNetwork.Train(trainingDataPoints);
