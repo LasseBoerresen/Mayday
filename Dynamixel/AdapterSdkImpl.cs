008f13bcbddf545af0f76ca6ac810fa742226536
LB <fiffigini@gmail.com>
2025-02-16 15:01:39 +0100

    ♻️ using explicit constructor in AdapterSdkImpl

--- Dynamixel/AdapterSdkImpl.cs
+++ Dynamixel/AdapterSdkImpl.cs
@@ -6,8 +6,17 @@ using UnitsNet;
 
 namespace Dynamixel;
 
-public class AdapterSdkImpl(PortAdapter portAdapter, JointStateCache JointStateCache) : Adapter
+public class AdapterSdkImpl : Adapter
 {
+    readonly PortAdapter _portAdapter;
+    readonly JointStateCache _jointStateCache;
+
+    public AdapterSdkImpl(PortAdapter portAdapter, JointStateCache jointStateCache)
+    {
+        _portAdapter = portAdapter;
+        _jointStateCache = jointStateCache;
+    }
+
     static int DXL_BROADCAST_ID = 254;
     
     static readonly Option<RotationalSpeed> VelocityLimitSlow = RotationalSpeed.FromRevolutionsPerSecond(0.5);  // AngularVelocity(tau / 8)  // tau / 16;
@@ -30,7 +39,7 @@ public class AdapterSdkImpl(PortAdapter portAdapter, JointStateCache JointStateC
         TorqueEnable(id);
         
         // Ensure a state value is always available post initialization. 
-        JointStateCache.SetFor(id, GetState(id));
+        _jointStateCache.SetFor(id, GetState(id));
     }
 
     public JointState GetState(JointId id)
@@ -45,28 +54,28 @@ public class AdapterSdkImpl(PortAdapter portAdapter, JointStateCache JointStateC
 
     Angle ReadAngle(JointId id)
     {
-        var positionSteps = portAdapter.Read(id, ControlRegister.PresentPosition);
+        var positionSteps = _portAdapter.Read(id, ControlRegister.PresentPosition);
         
         return StepAngle.ToAngle(positionSteps);
     }
 
     Angle ReadAngleGoal(JointId id)
     {
-        var positionSteps = portAdapter.Read(id, ControlRegister.GoalPosition);
+        var positionSteps = _portAdapter.Read(id, ControlRegister.GoalPosition);
         
         return StepAngle.ToAngle(positionSteps);
     }
 
     RotationalSpeed ReadSpeed(JointId id)
     {
-        var speedSteps = portAdapter.Read(id, ControlRegister.PresentVelocity);
+        var speedSteps = _portAdapter.Read(id, ControlRegister.PresentVelocity);
         
         return StepSpeed.ToSpeed(speedSteps);
     }
 
     LoadRatio ReadLoadRatio(JointId id)
     {
-        var loadSteps = portAdapter.Read(id, ControlRegister.PresentLoad);
+        var loadSteps = _portAdapter.Read(id, ControlRegister.PresentLoad);
         
         // TODO Test with real dynamixels, that -1000:1000 range is converted correctly, from uint to int...
         return LoadRatio.FromSteps((int)loadSteps);
@@ -74,14 +83,14 @@ public class AdapterSdkImpl(PortAdapter portAdapter, JointStateCache JointStateC
 
     UnitsNet.Temperature ReadTemperature(JointId id)
     {
-        var temperatureSteps = portAdapter.Read(id, ControlRegister.PresentTemperature);
+        var temperatureSteps = _portAdapter.Read(id, ControlRegister.PresentTemperature);
 
         return StepTemperature.ToTemperature(temperatureSteps);
     }
 
     void ReadHardwareErrorStatus(JointId id)
     {
-        var hardwareErrorStatus = portAdapter.Read(id, ControlRegister.HardwareErrorStatus);
+        var hardwareErrorStatus = _portAdapter.Read(id, ControlRegister.HardwareErrorStatus);
         if (hardwareErrorStatus != 0)
             Console.WriteLine($"HardwareErrorStatus: {hardwareErrorStatus:b8}");
     }
@@ -89,7 +98,7 @@ public class AdapterSdkImpl(PortAdapter portAdapter, JointStateCache JointStateC
     void Reboot(JointId id)
     {
         Console.WriteLine($"Rebooting {id}");
-        portAdapter.Reboot(id);
+        _portAdapter.Reboot(id);
         Thread.Sleep(300);
         
         var delay = TimeSpan.FromSeconds(0.1);
@@ -104,12 +113,12 @@ public class AdapterSdkImpl(PortAdapter portAdapter, JointStateCache JointStateC
 
     bool Ping(JointId id)
     {
-        return portAdapter.Ping(id);
+        return _portAdapter.Ping(id);
     }
 
     public void SetGoal(JointId id, Angle angle)
     {
-        portAdapter.Write(id, ControlRegister.GoalPosition, StepAngle.ToSteps(angle));
+        _portAdapter.Write(id, ControlRegister.GoalPosition, StepAngle.ToSteps(angle));
     }
 
     void SetVelocityLimit(JointId id)
@@ -118,7 +127,7 @@ public class AdapterSdkImpl(PortAdapter portAdapter, JointStateCache JointStateC
             .Map(DynamixelRotationalSpeed.FromRotationalSpeed)
             .IfNone(DynamixelRotationalSpeed.Infinite);
          
-        portAdapter.Write(id, ControlRegister.ProfileVelocity, dynamixelVelocity.Value);
+        _portAdapter.Write(id, ControlRegister.ProfileVelocity, dynamixelVelocity.Value);
     }
 
     void SetRotationDirection(JointId id, RobotDomain.Structures.RotationDirection rotationDirection)
@@ -130,12 +139,12 @@ public class AdapterSdkImpl(PortAdapter portAdapter, JointStateCache JointStateC
     
     uint GetDriveMode(JointId id)
     {
-        return portAdapter.Read(id, ControlRegister.DriveMode);
+        return _portAdapter.Read(id, ControlRegister.DriveMode);
     }
     
     void SetDriveMode(JointId id, uint driveMode)
     {
-        portAdapter.Write(id, ControlRegister.DriveMode, driveMode);
+        _portAdapter.Write(id, ControlRegister.DriveMode, driveMode);
     }
 
     void TorqueEnable(JointId id) => SetTorque(id, true);
@@ -144,11 +153,11 @@ public class AdapterSdkImpl(PortAdapter portAdapter, JointStateCache JointStateC
 
     void SetTorque(JointId id, bool value)
     {
-        portAdapter.Write(id, ControlRegister.TorqueEnable, Convert.ToUInt32(value));
+        _portAdapter.Write(id, ControlRegister.TorqueEnable, Convert.ToUInt32(value));
     }
 
     public void Dispose()
     {
-        portAdapter.Dispose();
+        _portAdapter.Dispose();
     }
 }
