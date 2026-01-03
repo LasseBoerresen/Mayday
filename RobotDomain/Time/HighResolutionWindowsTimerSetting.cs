d325b6efa63667dac61277209e090a941ae62e76
Lasse Børresen <lasse.boerresen@3shape.com>
2026-01-03 23:53:29 +0100

    ✨ Add `VerifyTimerResolution` method to ensure accurate resolution measurements.

--- RobotDomain/Time/HighResolutionWindowsTimerSetting.cs
+++ RobotDomain/Time/HighResolutionWindowsTimerSetting.cs
@@ -24,6 +24,23 @@ public class HighResolutionWindowsTimerSetting
     [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
     static extern uint TimeEndPeriod(uint uMilliseconds);
 
+    [DllImport("ntdll.dll", SetLastError = true)]
+    static extern int NtQueryTimerResolution(out uint minimumResolution, out uint maximumResolution, out uint currentResolution);
+    
+    static uint GetCurrentResolution()
+    {
+        NtQueryTimerResolution(out _, out _, out uint currentResolution);
+        return currentResolution; // Resolution is in 100-nanosecond units
+    }
+    
+    public void VerifyTimerResolution()
+    {
+        var current = GetCurrentResolution();
+        
+        // 1ms = 10,000 units of 100ns
+        Console.WriteLine($"Current Timer Resolution: {current / 10000.0} ms");
+    }
+    
     static void DisposeOnProcessExit()
     {
         AppDomain.CurrentDomain.ProcessExit += (s, e) => Instance.Dispose();
