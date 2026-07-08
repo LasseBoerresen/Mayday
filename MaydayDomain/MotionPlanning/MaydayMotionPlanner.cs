c194ad80a5d0e28416dfaaad580f72b8ab716523
LB <fiffigini@gmail.com>
2026-07-08 23:26:07 +0200

    🎨 Add spacing between inteface methods for readability.

--- MaydayDomain/MotionPlanning/MaydayMotionPlanner.cs
+++ MaydayDomain/MotionPlanning/MaydayMotionPlanner.cs
@@ -8,14 +8,23 @@ namespace MaydayDomain.MotionPlanning;
 public interface MaydayMotionPlanner : IDisposable
 {
     MaydayStructureSet<MaydayLegPosture> GetPostures();
+    
     MaydayLegPosture GetPostureOf(MaydayLegId legId);
+    
     void MoveTipPositions(Timed<MaydayStructureSet<Xyz>> tipDeltasTimed);
+    
     void SetTipPositionsForLegs(Timed<MaydayStructureSet<Xyz>> tipPositionsTimed);
+    
     void SetPosture(Timed<MaydayStructurePosture> postureTimed);
+    
     void SetPosture(Timed<MaydayLegPosture> postureTimed);
+    
     MaydayStructureSet<Xyz> GetPositionsOf(LinkName linkName);
+    
     Xyz GetPositionOf(LinkName linkName, MaydayLegId legId);
+    
     MaydayStructureSet<Q> GetOrientationsOf(LinkName linkName);
+    
     MaydayStructureSet<Transform> GetTransformsOf(LinkName linkName);
     
     /// <summary>
