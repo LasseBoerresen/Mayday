a7af017bba2536d9d6f51a38c300ea3363543715
LB <fiffigini@gmail.com>
2026-07-13 23:38:23 +0200

    🎉 Add EllieMain project and ellie class

---
+++ Ellie/EllieMain/Base/Ellie.cs
@@ -0,0 +1,12 @@
+﻿using LanguageExt;
+using RobotDomain.Behavior;
+
+namespace EllieMain.Base;
+
+public class Ellie(BehaviorController behaviorController, CancellationTokenSource cancelTokenSource)
+{
+    public Unit Start() => behaviorController.Start();
+
+    public void Stop() => cancelTokenSource.Cancel();
+}
+
