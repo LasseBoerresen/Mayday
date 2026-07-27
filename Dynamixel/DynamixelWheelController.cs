6b15df3f7853e7858824363767d4e1de295b94c5
LB <fiffigini@gmail.com>
2026-07-27 21:30:07 +0200

    ✨ Add unimplemented DynamixelWheelController

---
+++ Dynamixel/DynamixelWheelController.cs
@@ -0,0 +1,11 @@
+﻿using RobotDomain.Motion;
+
+namespace Dynamixel;
+
+public class DynamixelWheelController : WheelController 
+{
+    public void Initialize(WheelId id, RobotDomain.Structures.RotationDirection rotationDirection)
+    {
+        throw new NotImplementedException("Dynamixel wheel controller not implemented yet.");
+    }
+}
